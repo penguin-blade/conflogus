c8afc0ccf927de3342dc89b5881378000e555614
Darling <stormofdark@libero.it>
2014-12-12 16:47:30 +0100

    First Poject files
    Basic bootstrap template without functions.

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<div class="navbar navbar-default" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="pull-left" href="#"><img alt="ConfLogus" src="images/conflogus_logo.png"></a>
+        </div>
+        <center>   
+                <form class="navbar-form navbar-right" role="search" style="margin-top: 13px;">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="username" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="password" placeholder="Password">
+                    </div>
+                    <button type="submit" class="btn btn-default">Sign In</button>
+                </form>
+            </div>
+        </center>
+    </div>
+</div>
